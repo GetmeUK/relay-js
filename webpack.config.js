1c1c4b913b4b41dc4acaa9c976aeba6c34dcdd7a
Anthony Blackshaw <ant@getme.co.uk>
2021-11-18 00:50:39 +0000

    webpack config added

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+
+
+module.exports = {
+    entry: {
+        'index': './module/index.js'
+    },
+
+    externals: {},
+
+    output: {
+        library: 'relay-js',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname, 'umd'),
+        filename: '[name].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+
+            // -- Pre --
+
+            // JS lint
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    stats: {
+        colors: true
+    }
+};
